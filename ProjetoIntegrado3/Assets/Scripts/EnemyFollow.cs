61adc9a49361e468df9a6add604fc7682a1a9bfd
Kaue <uriel.kau39@gmail.com>
2020-03-12 17:15:36 -0300

    NavMesh instalado

---
+++ ProjetoIntegrado3/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform Player;
+    public Transform[] RandomPoints;
+    public float FieldofView = 30;
+    public float FollowDistance = 20;
+    public float AttackDistance = 2;
+    public float PatrolSpeed = 3;
+    public float ChasingSpeed = 6;
+    public float AttackTime = 1.5f;
+    public float Damage = 100;
+
+
+    private NavMeshAgent NaveMesh;
+    private float PlayerDistance;
+    private float WayPointDistance;
+    private float ChaseChronom;
+    private float AttackChronom;
+    private int AtualWayPoint;
+    private bool VisiblePlayer;
+    private bool ChasingSomething;
+    private bool ContChasingSomething;
+    private bool AttackingSomething;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AtualWayPoint = Random.Range(0, RandomPoints.Length);
+        NaveMesh = transform.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerDistance = Vector3.Distance(Player.transform.position,transform.position);
+        WayPointDistance = Vector3.Distance(RandomPoints[AtualWayPoint].transform.position, transform.position);
+
+
+        RaycastHit hit;
+
+        Vector3 deOnde = transform.position;
+        Vector3 paraOnde = Player.transform.position;
+        Vector3 Direction = paraOnde - deOnde;
+
+        if(Physics.Raycast(transform.position,Direction,out hit,1000) && PlayerDistance < FieldofView)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                VisiblePlayer = true;
+            }
+            else
+            {
+                VisiblePlayer = false;
+            }
+
+        }
+
+        if (PlayerDistance > FieldofView)
+        {
+            Patrol(); 
+        }
+
+        if(PlayerDistance<=FieldofView && PlayerDistance > FollowDistance)
+        {
+            if (VisiblePlayer == true)
+            {
+                Look();
+            }
+            else
+            {
+                Patrol();
+            }
+        }
+        if(PlayerDistance<=FollowDistance && PlayerDistance > AttackDistance)
+        {
+            if (VisiblePlayer == true)
+            {
+                Follow();
+                ChasingSomething = true;
+            }
+            else
+            {
+                Patrol();
+            }
+        }
+        if (PlayerDistance <= AttackDistance)
+        {
+            Attack();
+        }
+
+        if (WayPointDistance <= 2)
+        {
+            AtualWayPoint = Random.Range(0, RandomPoints.Length);
+            Patrol();
+        }
+
+        if (ContChasingSomething == true)
+        {
+            ChaseChronom += Time.deltaTime;
+        }
+
+        if(ChaseChronom>=5 && VisiblePlayer == false)
+        {
+            ChasingSomething = false;
+            ContChasingSomething = false;
+            ChaseChronom = 0;
+        }
+
+        if (AttackingSomething == true)
+        {
+            AttackChronom += Time.deltaTime;
+        }
+
+        if(AttackChronom>=AttackTime && PlayerDistance <= AttackDistance )
+        {
+            AttackingSomething = true;
+            AttackChronom = 0;
+            PlayerLife.Life = PlayerLife.Life - Damage;
+            Debug.Log("Recebeu Ataque");
+;        }
+            else if(AttackChronom >= AttackTime && PlayerDistance > AttackDistance)
+            {
+            AttackingSomething = false;
+            AttackChronom = 0;
+            Debug.Log("Errou");
+            }
+    }
+
+    void Patrol()
+    {
+        if (ChasingSomething == false)
+        {
+            NaveMesh.acceleration = 5;
+            NaveMesh.speed = PatrolSpeed;
+            NaveMesh.destination = RandomPoints[AtualWayPoint].position;
+;        }
+        else if (ChasingSomething == true)
+        {
+            ContChasingSomething = true;
+        }
+    }
+
+    void Look()
+    {
+        NaveMesh.speed = 0;
+        transform.LookAt(Player);
+    }
+
+    void Follow()
+    {
+        NaveMesh.acceleration = 8;
+        NaveMesh.speed = ChasingSpeed;
+        NaveMesh.destination = Player.position;
+    }
+
+    void Attack()
+    {
+        AttackingSomething = true;
+    }
+}
--- ProjetoIntegrado3/Assets/Scripts/ThirdPersonUserControl.cs
+++ ProjetoIntegrado3/Assets/Scripts/ThirdPersonUserControl.cs
@@ -8,25 +8,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     public class ThirdPersonUserControl : MonoBehaviour
     {
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
         
         private void Start()
         {
-            // get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }
+           
 
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
@@ -46,25 +34,16 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
-
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
-            }
+            float h = CrossPlatformInputManager.GetAxis("Horizontal")*0.5f;
+            float v = CrossPlatformInputManager.GetAxis("Vertical") * 0.5f;
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+             
+           
+            m_Move = v*transform.forward + h*transform.right;
+          
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 2.0f;
 #endif
 
             // pass all parameters to the character control script
