5d428d547e1ada22ad2e9b10416af64644adc877
Kaue <uriel.kau39@gmail.com>
2020-03-17 17:42:42 -0300

    Navmesh do inimigo realizado com sucesso

--- ProjetoIntegrado3/Assets/Scripts/Enemy.cs
+++ ProjetoIntegrado3/Assets/Scripts/Enemy.cs
@@ -6,11 +6,18 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     public GameObject player;
+    public Transform eyes;
     public AudioClip[] footsounds;
+    public AudioSource growl;
 
     private NavMeshAgent nav;
     private AudioSource sound;
     private Animator anim;
+    private string state = "idle";
+    private bool alive = true;
+    private bool highAlert = false;
+    private float wait = 0f;
+    private float alertness = 100;
 
 
     // Start is called before the first frame update
@@ -19,8 +26,8 @@ public class Enemy : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
         sound = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
-        nav.speed = 1.2f;
-        anim.speed = 1.2f;
+        nav.speed = 1f;
+        anim.speed = 1f;
     }
 
     public void footstep(int _num)
@@ -29,12 +36,127 @@ public class Enemy : MonoBehaviour
         sound.Play();
     }
 
-    // Update is called once per frame
+    //Checagem para saber se está vendo o player
+    public void checkSight()
+    {
+        if (alive)
+        {
+            RaycastHit rayHit;
+            if(Physics.Linecast(eyes.position,player.transform.position,out rayHit))
+            {
+                print("hit " + rayHit.collider.gameObject.name);
+                if (rayHit.collider.gameObject.name == "Player")
+                {
+                    if (state != "kill")
+                    {
+                        state = "chase";
+                        nav.speed=2.5f;
+                        anim.speed=2.5f;
+                        growl.pitch = 1.2f;
+                        growl.Play();
+                    }
+                }
+
+
+            }
+;
+        }
+    }
+
+    
     void Update()
     {
-        anim.SetFloat("velocity", nav.velocity.magnitude);
+        if (alive)
+        {
+            anim.SetFloat("velocity", nav.velocity.magnitude);
 
-        nav.SetDestination(player.transform.position);
+            //Idle//
+
+            if (state == "idle")
+            {
+                //escolher um lugar aleatório para ir
+                Vector3 randomPos = Random.insideUnitSphere * alertness;
+                NavMeshHit navHit;
+                NavMesh.SamplePosition(transform.position + randomPos, out navHit, 100, NavMesh.AllAreas);
+
+                //fica rodando ao redor do player
+                if (highAlert)
+                {
+                    NavMesh.SamplePosition(player.transform.position + randomPos, out navHit, 40f, NavMesh.AllAreas);
+
+                    //ao passar do tempo ele começa a perder a posição do player
+
+                    alertness += 5f;
+                    if (alertness > 40f)
+                    {
+                        highAlert = false;
+                        nav.speed = 1.2f;
+                        anim.speed = 1.2f;
+                    }
+                }
+
+                nav.SetDestination(navHit.position);
+                state = "walk";
+            }
+
+            //Walk//
+
+            if (state == "walk")
+            {
+                if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending)
+                {
+                    state = "search";
+                    wait = 5f;
+                }
+            }
+            //Search//
+
+            if (state == "search")
+            {
+                if (wait >  0f)
+                {
+                    wait -= Time.deltaTime;
+                    transform.Rotate(0f, 30f * Time.deltaTime, 0f);
+
+                }
+                else
+                {
+                    state = "idle";
+                }
+            }
+
+            //Chase//
+
+            if (state == "chase")
+            {
+                nav.destination = player.transform.position;
+
+                //perdeu a visão do Player//
+                float distance = Vector3.Distance(transform.position, player.transform.position);
+                if (distance > 10f)
+                {
+                    state = "hunt";
+                }
+;           }
+
+            //Hunt//
+
+            if (state == "hunt")
+            {
+                if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending)
+                {
+                    state = "search";
+                    wait = 5f;
+                    highAlert = true;
+                    alertness = 5f;
+                    checkSight();
+                }
+            }
+
+
+            //nav.SetDestination(player.transform.position);
+        }
+
     }
 
   
---
+++ ProjetoIntegrado3/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool alive = true;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Eyes")
+        {
+            other.transform.parent.GetComponent<Enemy>().checkSight();
+        }
+    }
+}
